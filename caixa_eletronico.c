077f95f0fc7563e5556d1e009138447c0f3db530
Catarine Sales <catarinesales2013@gmail.com>
2026-06-21 20:13:57 -0300

    Primeiro Commit

---
+++ caixa_eletronico.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+   float saldo = 1000.00;
+    
+   printf("Boa noite!\n");
+   printf(" Seu Saldo inicial é de R$ %.2f reais.\n",saldo);
+
+}
